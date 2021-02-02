4ae6dafc7b32d8f4ff698f7ef28264ad94ac7dd0
Troy Matthews <Troy-Matthews@hotmail.com>
2021-02-02 17:19:35 +0000

    created container table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_150801) do
+ActiveRecord::Schema.define(version: 2021_02_02_171258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "containers", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "display_name"
---
+++ spec/factories/containers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :container do
+    Title { "MyString" }
+    description { "MyText" }
+  end
+end
