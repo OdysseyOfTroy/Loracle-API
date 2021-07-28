17f7805895a982449c1802115ab2acc2c2447960
troy <mrtroymatthews@gmail.com>
2021-07-28 20:22:02 +0100

    more bugtesting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::API
     private
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :display_name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [ :display_name])
     end
   
     def authenticate_user
